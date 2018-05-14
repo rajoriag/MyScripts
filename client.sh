027cb6aec15b86803918d0f0876666e977585f90
Girjesh Rajoria <32087575+grajoria@users.noreply.github.com>
2018-05-14 17:45:43 +0530

    Update client.sh

--- client.sh
+++ client.sh
@@ -4,8 +4,6 @@
 #  - SERVER: hostname or IP-address of the NFS-server
 #  - EXPORT: NFS-export to test (should start with "/")
 
-echo "Client Script"
-
 # if any command fails, the script should exit
 set -e
 
@@ -15,88 +13,167 @@ set -x
 [ -n "${SERVER}" ]
 [ -n "${EXPORT}" ]
 
-# install build and runtime dependencies
-echo "Install runtime dependencies"
-yum -y install nfs-utils
-
-# dbench is available from the testing repositories in the CentOS Storage SIG
-yum -y install centos-release-gluster
-yum -y --enablerepo=centos-gluster*-test install dbench
-
-# place all used files in ${WORKDIR}
-WORKDIR=/var/tmp/dbench.d
-mkdir ${WORKDIR}
-
-curl -o ${WORKDIR}/client.txt https://raw.githubusercontent.com/sahlberg/dbench/master/loadfiles/client.txt
-
-
-# v3 mount
-mkdir -p /mnt/nfsv3
-mount -t nfs -o vers=3 ${SERVER}:${EXPORT} /mnt/nfsv3
-mkdir /mnt/nfsv3/v3
-
-# Running dbench suite on v3 mount
-echo "---------------------------------------"
-echo "dbench Test Running for v3 Mount..."
-echo "---------------------------------------"
-dbench --directory=/mnt/nfsv3/v3 --loadfile=${WORKDIR}/client.txt 2 > ${WORKDIR}/dbenchTestLog.txt
-tail -1 ${WORKDIR}/dbenchTestLog.txt | grep "Throughput"
-status=$?
-if [ $status -eq 0 ]
+if [ "$1" = "client_initialization" ]
 then
-      tail -21 ${WORKDIR}/dbenchTestLog.txt
-      echo "dbench Test: SUCCESS"
-else
-      tail -5 ${WORKDIR}/dbenchTestLog.txt
-      echo "dbench Test: FAILURE"
-      exit $status
+	# install build and runtime dependencies
+	yum -y install nfs-utils time
+	mkdir -p /mnt/ganesha
+	
+	echo "------------------------------------------------------------------------"
+	echo "Client Initial Stage --- With All Rights To All Clients ( RO & RW ) "
+	echo "------------------------------------------------------------------------"
+	
+	mount -t nfs -o vers=3 ${SERVER}:${EXPORT} /mnt/ganesha
+	cd /mnt/ganesha
+	echo "Trying To Write A File"
+	echo "Hello World" > testFile.txt
+	ret=$?
+	if [ $ret -ne 0 ]
+	then
+		echo "FAILURE: Write permissions denied"
+		exit $ret
+	fi
+	echo "Trying To Read A File"
+	cat testFile.txt
+	ret=$?
+	if [ $ret -ne 0 ]
+	then
+		echo "FAILURE: Read permissions denied"
+		exit $ret
+	fi
+	echo "SUCCESS: With all rights to all Clients ( RO & RW )"
+	#unmount
+	cd / && umount -l /mnt/ganesha
 fi
-umount -l /mnt/nfsv3
-      
-      
-# v4 mount
-mkdir -p /mnt/nfsv4
-mount -t nfs -o vers=4.0 ${SERVER}:${EXPORT} /mnt/nfsv4
-mkdir /mnt/nfsv4/v4
 
-# Running dbench suite on v4.0 mount
-echo "---------------------------------------"
-echo "dbench Test Running for v4.0 Mount..."
-echo "---------------------------------------"
-dbench --directory=/mnt/nfsv4/v4 --loadfile=${WORKDIR}/client.txt 2 > ${WORKDIR}/dbenchTestLog.txt
-tail -1 ${WORKDIR}/dbenchTestLog.txt | grep "Throughput"
-status=$?
-if [ $status -eq 0 ]
+if [ "$1" = "client_stage1" ]
 then
-      tail -21 ${WORKDIR}/dbenchTestLog.txt
-      echo "dbench Test: SUCCESS"
-else
-      tail -5 ${WORKDIR}/dbenchTestLog.txt
-      echo "dbench Test: FAILURE"
-      exit $status
+	echo "------------------------------------------------------------------------"
+	echo "Client Stage 1 --- With Only RO Rights To This Client "
+	echo "------------------------------------------------------------------------"
+
+	mount -t nfs -o vers=3 ${SERVER}:${EXPORT} /mnt/ganesha
+	cd /mnt/ganesha
+	echo "Trying To Write A File"
+	sed -i '1s/$/ From RedHat/' testFile.txt
+	ret=$?
+	if [ $ret -eq 0 ]
+	then
+		echo "FAILURE: Write permissions were not blocked to the Client"
+		exit -1
+	fi
+	echo "Trying To Read A File"
+	cat testFile.txt
+	ret=$?
+	if [ $ret -ne 0 ]
+	then
+		echo "FAILURE: Read permissions denied"
+		exit $ret
+	fi
+	echo "SUCCESS: With Only RO Rights To This Client"
+	# unmount
+	cd / && umount -l /mnt/ganesha
 fi
-umount -l /mnt/nfsv4
 
 
-# v4.1 mount
-mkdir -p /mnt/nfsv4_1
-mount -t nfs -o vers=4.1 ${SERVER}:${EXPORT} /mnt/nfsv4_1
-mkdir /mnt/nfsv4_1/v41
-
-# Running dbench suite on v4.1 mount
-echo "---------------------------------------"
-echo "dbench Test Running for v4.1 Mount..."
-echo "---------------------------------------"
-dbench --directory=/mnt/nfsv4_1/v41 --loadfile=${WORKDIR}/client.txt 2 > ${WORKDIR}/dbenchTestLog.txt
-tail -1 ${WORKDIR}/dbenchTestLog.txt | grep "Throughput"
-status=$?
-if [ $status -eq 0 ]
+if [ "$1" = "client_stage2" ]
 then
-      tail -21 ${WORKDIR}/dbenchTestLog.txt
-      echo "dbench Test: SUCCESS"
-else
-      tail -5 ${WORKDIR}/dbenchTestLog.txt
-      echo "dbench Test: FAILURE"
-      exit $status
+	echo "------------------------------------------------------------------------"
+	echo "Client Stage 2 --- With Only Rights For v3 Mount To This Client "
+	echo "------------------------------------------------------------------------"
+
+	echo "Trying To Mount By vers=3"
+	mount -t nfs -o vers=3 ${SERVER}:${EXPORT} /mnt/ganesha
+	ret=$?
+	if [ $ret -ne 0 ]
+	then
+		echo "FAILURE: Mount v3 failed"
+		exit $ret
+	else
+		#unmount version 3 
+		cd / && umount -l /mnt/ganesha
+	fi
+	
+	echo "Trying To Mount By vers=4.0"
+	mount -t nfs -o vers=4.0 ${SERVER}:${EXPORT} /mnt/ganesha
+	ret=$?
+	if [ $ret -eq 0 ]
+	then
+		echo "FAILURE: Mount v4.0 Permissions were not blocked to the Client"
+		exit -1
+	fi
+
+	echo "Trying To Mount By vers=4.1"
+	mount -t nfs -o vers=4.1 ${SERVER}:${EXPORT} /mnt/ganesha
+	ret=$?
+	if [ $ret -eq 0 ]
+	then
+		echo "FAILURE: Mount v4.1 permissions were not blocked to the Client"
+		exit -1
+	fi
 fi
-umount -l /mnt/nfsv4_1
+
+if [ "$1" = "client_stage3" ]
+then
+	echo "-----------------------------------------------------------------------------"
+	echo "Client Stage 3 --- With Only Rights For v4.0 & v4.1 Mount To This Client "
+	echo "-----------------------------------------------------------------------------"
+
+	echo "Trying To Mount By vers=3"
+	mount -t nfs -o vers=3 ${SERVER}:${EXPORT} /mnt/ganesha
+	ret=$?
+	if [ $ret -eq 0 ]
+	then
+		echo "FAILURE: Mount v3 permissions were not blocked to the Client"
+		exit -1
+	fi
+
+	echo "Trying To Mount By vers=4.0"
+	mount -t nfs -o vers=4.0 ${SERVER}:${EXPORT} /mnt/ganesha
+	ret=$?
+	if [ $ret -ne 0 ]
+	then
+		echo "FAILURE: Mount v4.0 failed"
+		exit $ret
+	else
+		#unmount version 4.0
+		cd / && umount -l /mnt/ganesha
+	fi
+
+	echo "Trying To Mount By vers=4.1"
+	mount -t nfs -o vers=4.1 ${SERVER}:${EXPORT} /mnt/ganesha
+	ret=$?
+	if [ $ret -ne 0 ]
+	then
+		echo "FAILURE: Mount v4.1 failed"
+		exit $ret
+	else
+		#unmount version 4.1
+		cd / && umount -l /mnt/ganesha
+	fi
+fi
+
+if [ "$1" = "client_stage4" ]
+then
+	echo "-----------------------------------------------------------------------------"
+	echo "Client Stage 4 --- With Squashed Root Mount To This Client "
+	echo "-----------------------------------------------------------------------------"
+
+	mount -t nfs ${SERVER}:${EXPORT} /mnt/ganesha
+
+	echo "Creating New User : test-user"
+	adduser test-user
+	echo asd123 | passwd test-user --stdin
+
+	echo "Adding test-user to sudoers file"
+	echo -e 'test-user \t ALL=(ALL) \t NOPASSWD:ALL' >> /etc/sudoers
+
+	echo "Trying To Change Ownership Of The File testFile.txt in the mount"
+	sudo chown test-user /mnt/ganesha/testFile.txt
+	ret=$?
+	if [ $ret -eq 0 ]
+	then
+		echo "FAILURE: ROOT permissions were not blocked the Client"
+		exit -1
+	fi
+fi
